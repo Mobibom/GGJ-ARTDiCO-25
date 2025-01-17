6373141f597461335a4f041586d68791a1b1bd02
WangSimiao2000 <1872557359@qq.com>
2025-01-18 03:31:22 +0800

    创建readme.md, 场景里更新UI, 创建一个SecretDetailManager脚本

---
+++ Assets/Scripts/SecretDetailManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretDetailManager : MonoBehaviour
+{
+    public void ShowPicSprite()
+    {
+        // TODO:
+        // 1. ÔØÈëĞÂµÄÍ¼Æ¬
+        // 2. ÏÔÊ¾ShowSecretsDetailÍ¼Æ¬
+        // 3. Öğ½¥·Å´óÍ¼Æ¬
+
+    }
+
+    public void HidePicSprite()
+    {
+        // TODO:
+        // 1. Öğ½¥ËõĞ¡Í¼Æ¬
+        // 2. Òş²ØShowSecretsDetailÍ¼Æ¬
+    }
+}
